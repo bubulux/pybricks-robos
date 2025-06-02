1cc2fe2855278a16ec75e05a384ba557b5ad32b6
Samuil Berenzon <samus.dev.space@gmail.com>
2025-06-02 17:46:57 +0200

    with reverse

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from pybricks.parameters import Port
+from pybricks.parameters import Port, Direction
 from pybricks.pupdevices import Motor
 from pybricks.tools import wait
 
-motor = Motor(Port.A)
+rightMotor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+leftMotor = Motor(Port.E)
 
-motor.run(200)
+rightMotor.run(300)
+leftMotor.run(300)
 
-while True:
-    pass
+wait(3 * 1000)
 
-# wait(10 * 1000)
+rightMotor.run(-300)
+leftMotor.run(-300)
+
+wait(3 * 1000)
+
+rightMotor.run(300)
+leftMotor.run(-300)
+
+wait(3 * 1000)
