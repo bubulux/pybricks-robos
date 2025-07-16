0a3475d1eaffbf1f478367c95b9447746269b3c6
Samuil Berenzon <samus.dev.space@gmail.com>
2025-07-16 23:01:24 +0200

    init broadcast / observer

---
+++ broadcast.py
@@ -0,0 +1,24 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port
+from pybricks.tools import wait
+
+# Initialize the hub.
+hub = PrimeHub(broadcast_channel=1)
+
+# Initialize the motors.
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.B)
+
+while True:
+    # Read the motor angles to be sent to the other hub.
+    left_angle = left_motor.angle()
+    right_angle = right_motor.angle()
+
+    # Set the broadcast data and start broadcasting if not already doing so.
+    data = (left_angle, right_angle)
+    hub.ble.broadcast(data)
+
+    # Broadcasts are only sent every 100 milliseconds, so there is no reason
+    # to call the broadcast() method more often than that.
+    wait(100)
