3aa49894b68e671389aa724ccb4a2cf3a943c4e5
Samuil Berenzon <samus.dev.space@gmail.com>
2025-09-11 21:07:16 +0200

    run forward / backward

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pybricks.hubs import PrimeHub
 from pybricks.parameters import Direction, Port
 from pybricks.pupdevices import ColorSensor, ForceSensor, Motor
 from pybricks.tools import wait
+from pybricks.iodevices import XboxController
 
 # Set up all devices.
 prime_hub = PrimeHub()
@@ -9,8 +10,9 @@ right_feel = ForceSensor(Port.C)
 left_feel = ForceSensor(Port.D)
 back_feel = ForceSensor(Port.A)
 color_sensor = ColorSensor(Port.B)
-right = Motor(Port.E, Direction.CLOCKWISE)
+right = Motor(Port.E, Direction.COUNTERCLOCKWISE)
 left = Motor(Port.F, Direction.CLOCKWISE)
+controller = XboxController()
 
 
 # The main program starts here.
@@ -18,16 +20,42 @@ left = Motor(Port.F, Direction.CLOCKWISE)
 # right.run(500)
 # left.run(500)
 
-def display_pixel():
-    prime_hub.display.pixel()
+# def display_pixel():
+#     prime_hub.display.pixel(0,0,100)
+
+# display_pixel()
+
+def isForward():
+    lt = controller.triggers()[0]
+    if lt > 0:
+        return True
+    return False
+
+def isBackward():
+    rt = controller.triggers()[1]
+    if rt > 0:
+        return True
+    return False
+
+def isStop():
+    if isForward() == False and isBackward() == False:
+        return True
+
+def runForward():
+    left.run(500)
+    right.run(500)
+
+def runBackward():
+    left.run(-500)
+    right.run(-500)
 
 while True:
-    # print(f"Color: {color_sensor.hsv()}")
-    # print(f"Right PRESSED: {right_feel.pressed()}")
-    # print(f"Right TOUCHED: {right_feel.touched()}")
-    # print(f"Left PRESSED: {left_feel.pressed()}")
-    # print(f"Left TOUCHED: {left_feel.touched()}")
-    # print(f"Back PRESSED: {back_feel.pressed()}")
-    # print(f"Back TOUCHED: {back_feel.touched()}")
-    wait(1000)
+    if isForward():
+        runForward()
+    elif isBackward():
+        runBackward()
+    elif isStop():
+        left.stop()
+        right.stop()
+    wait(50)
     pass
