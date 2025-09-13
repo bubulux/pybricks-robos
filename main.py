2c3317ebf6c8f623a4440050be23dbfb723e0a8e
Samuil Berenzon <samus.dev.space@gmail.com>
2025-09-13 12:18:35 +0200

    reset

--- main.py
+++ main.py
@@ -12,52 +12,8 @@ back_feel = ForceSensor(Port.A)
 color_sensor = ColorSensor(Port.B)
 right = Motor(Port.E, Direction.COUNTERCLOCKWISE)
 left = Motor(Port.F, Direction.CLOCKWISE)
-controller = XboxController()
 
-
-# The main program starts here.
-
-# right.run(500)
-# left.run(500)
-
-# def display_pixel():
-#     prime_hub.display.pixel(0,0,100)
-
-# display_pixel()
-
-def isForward():
-    lt = controller.triggers()[0]
-    if lt > 0:
-        return True
-    return False
-
-def isBackward():
-    rt = controller.triggers()[1]
-    if rt > 0:
-        return True
-    return False
-
-def isStop():
-    if isForward() == False and isBackward() == False:
-        return True
-
-def runForward():
-    print("Running forward")
-    left.run(500)
-    right.run(500)
-
-def runBackward():
-    print("Running backward")
-    left.run(-500)
-    right.run(-500)
 
 while True:
-    if isForward():
-        runForward()
-    elif isBackward():
-        runBackward()
-    elif isStop():
-        left.stop()
-        right.stop()
-    wait(50)
+   
     pass
