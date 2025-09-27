adba95bf8a8d360850b4d39ea54fc68997136a65
Samuil Berenzon <samus.dev.space@gmail.com>
2025-09-27 10:32:05 +0200

    matine

--- ui/src/renderer/src/App.tsx
+++ ui/src/renderer/src/App.tsx
@@ -1,9 +1,18 @@
 import { useStream } from "./stream";
+import { AppShell, Flex, Title } from "@mantine/core";
 
 function App(): React.JSX.Element {
-  const { data, history } = useStream();
+  const { data: streamData } = useStream();
 
-  return <div>Hello World</div>;
+  // Sample data for the chart
+  const sampleData = [
+    { date: "1", value: 10 },
+    { date: "2", value: 15 },
+    { date: "3", value: 7 },
+    { date: "4", value: 20 },
+  ];
+
+  return <AppShell></AppShell>;
 }
 
 export default App;
--- ui/src/renderer/src/main.tsx
+++ ui/src/renderer/src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
+
+import "@mantine/core/styles.css";
+import "@mantine/charts/styles.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MantineProvider
+      defaultColorScheme="dark"
+      theme={{
+        primaryColor: "indigo",
+      }}
+    >
+      <App />
+    </MantineProvider>
   </StrictMode>,
 );
