27337acaccb7da58dda27b41475dc8bd9d5d5840
Samuil Berenzon <samus.dev.space@gmail.com>
2025-09-27 10:58:29 +0200

    established layout

--- ui/src/renderer/src/App.tsx
+++ ui/src/renderer/src/App.tsx
@@ -1,18 +1,66 @@
 import { useStream } from "./stream";
-import { AppShell, Flex, Title } from "@mantine/core";
+import { AppShell, Flex, Divider } from "@mantine/core";
 
 function App(): React.JSX.Element {
   const { data: streamData } = useStream();
 
-  // Sample data for the chart
-  const sampleData = [
-    { date: "1", value: 10 },
-    { date: "2", value: 15 },
-    { date: "3", value: 7 },
-    { date: "4", value: 20 },
-  ];
-
-  return <AppShell></AppShell>;
+  return (
+    <AppShell>
+      <Flex
+        direction={"column"}
+        style={{
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <Flex
+          style={{
+            height: "70%",
+          }}
+        >
+          <Flex
+            style={{
+              width: "20%",
+            }}
+          >
+            FieldStatus
+          </Flex>
+          <Divider size="lg" color="blue" orientation="vertical" />
+          <Flex flex={1}>HealthMonitor</Flex>
+          <Divider size="lg" color="blue" orientation="vertical" />
+          <Flex
+            style={{
+              width: "20%",
+            }}
+          >
+            GameTracker
+          </Flex>
+        </Flex>
+        <Divider size="lg" color="blue" />
+        <Flex
+          style={{
+            height: "30%",
+          }}
+        >
+          <Flex flex={1}>
+            <Flex style={{ width: "10%" }}>MonitorConnection</Flex>
+            <Divider size="lg" color="blue" orientation="vertical" />
+            <Flex direction={"column"} style={{ width: "90%" }}>
+              <Flex
+                style={{
+                  height: "80%",
+                }}
+              >
+                HealthHistory
+              </Flex>
+              <Divider size="lg" color="blue" />
+              <Flex>DataHistory</Flex>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
+    </AppShell>
+  );
 }
 
 export default App;
