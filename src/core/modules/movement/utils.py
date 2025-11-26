c7dd6905505edfec3766e551fcc7f73161f17257
Samuil Berenzon <samus.dev.space@gmail.com>
2025-11-26 20:08:38 +0100

    baseline

--- src/core/modules/movement/utils.py
+++
@@ -1,4 +0,0 @@
-def convertPercentToDegreesPerSecond(speed: int) -> int:
-    base = 1000
-    percentile = base / 100
-    return int(speed * percentile)
--- src/main.py
+++ src/main.py
@@ -1,38 +1,10 @@
 from pybricks.hubs import PrimeHub
-from pybricks.iodevices import XboxController
 from pybricks.parameters import Direction, Port
-from pybricks.pupdevices import ColorSensor, ForceSensor, Motor
+from pybricks.pupdevices import Motor
 
+rightMotor = Motor(Port.D)
+leftMotor = Motor(Port.C, Direction.COUNTERCLOCKWISE)
 
-from core.index import Core
-from io.controller import Controller
-
-from eventLoop import EventLoop
-
-
-# Set up all devices.
-hub = PrimeHub()
-rightFeel = ForceSensor(Port.C)
-leftFeel = ForceSensor(Port.D)
-backFeel = ForceSensor(Port.A)
-colorSensor = ColorSensor(Port.B)
-rightMotor = Motor(Port.E, Direction.CLOCKWISE)
-leftMotor = Motor(Port.F, Direction.COUNTERCLOCKWISE)
-XBoxController = XboxController()
-
-refreshRate = 50  # milliseconds
-
-core = Core(
-    refreshRate,
-    hub,
-    rightFeel,
-    leftFeel,
-    backFeel,
-    colorSensor,
-    rightMotor,
-    leftMotor,
-)
-
-controller = Controller(XBoxController)
-
-EventLoop(core, controller, refreshRate).run()
+while True:
+    rightMotor.run(500)
+    leftMotor.run(500)
