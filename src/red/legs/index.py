6fda842dd4369c047d78d6b27a33b5cf818ee081
Samuil Berenzon <samus.dev.space@gmail.com>
2026-01-19 19:02:12 +0100

    body movement left / right

--- src/red/legs/index.py
+++ src/red/legs/index.py
@@ -1,3 +1,4 @@
+from pybricks.hubs import PrimeHub
 from pybricks.parameters import Direction, Port
 from pybricks.iodevices import XboxController
 from pybricks.pupdevices import Motor
@@ -6,20 +7,39 @@ from pybricks.tools import wait
 from common.movement.index import Movement
 from common.controller.index import Controller
 
+hub = PrimeHub(broadcast_channel=1)
+
 leftMotor = Motor(
     Port.A,
 )
 rightMotor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 body = Motor(Port.D)
 
-controller = XboxController()
+controllerO = XboxController()
 
 
 movement = Movement(leftMotor, rightMotor)
-controller = Controller(controller)
+controller = Controller(controllerO)
 
 while True:
     wait(100)
+
+    # 1 UP
+    # 7 LEFT
+    # 3 RIGHT
+    # 5 DOWN
+    dPadDir = controllerO.dpad()
+
+    if dPadDir == 0:
+        body.stop()
+    if dPadDir == 1:
+        hub.ble.broadcast("up")
+    elif dPadDir == 3:
+        body.run(300)
+    elif dPadDir == 5:
+        hub.ble.broadcast("down")
+    elif dPadDir == 7:
+        body.run(-300)
 
     isForward, forwardPercent = controller.forward()
     isBackward, backwardPercent = controller.backward()
